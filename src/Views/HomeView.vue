b3798166deccea822dca42768f4b9fdfb9598451
lgeti <ignat.pocevski@gmail.com>
2024-08-29 18:42:42 +0200

    Created the HomeView and started implementing some of the styles

---
+++ src/Views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container" id="home">
+        <h1 class="center">Are you ready?</h1>
+        <a href="/game.html" class="button center">Play</a>
+        <a href="/highscores.html" id="highscore-button" class="button center">High Scores<i
+                class="fas fa-crown"></i></a>
+    </div>
+</template>
+
+<style scoped>
+.center {
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+</style>
