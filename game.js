d67f279f62e89d5b8660e62eed70f5af6939bbcb
lgeti <ignat.pocevski@gmail.com>
2024-09-04 18:09:02 +0200

    Update game.js to fetch quiz questions from JSON file

--- game.js
+++ game.js
@@ -17,9 +17,6 @@ let score = 0;
 
 gradeBtns.classList.add("hidden");
 
-const myRequest = new Request("./src/data/24-2r-t.json");
-
-
 function fetchJSONFile(path) {
 	fetch(path)
 		.then((response) => {
@@ -116,80 +113,4 @@ function decrementScore() {
 	getNewQuestion();
 }
 
-
-fetchJSONFile("./src/data/24-1r-t.json");
-
-
-
-// let currentQuestion = {};
-// let acceptingAnswers = true;
-// let score = 0;
-// let questionCounter = 0;
-// let availableQuestions = [];
-
-// const SCORE_POINTS = 100;
-// const MAX_QUESTIONS = 4;
-
-// startGame = () => {
-// 	questionCounter = 0;
-// 	score = 0;
-// 	// availableQuestions = [...questions];
-// 	console.log(availableQuestions);
-// 	// getNewQuestion();
-// };
-
-// getNewQuestion = () => {
-// 	if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
-// 		localStorage.setItem("mostRecentScore", score);
-
-// 		return window.location.assign("/end.html");
-// 	}
-
-// 	questionCounter++;
-// 	progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-// 	progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
-// 	const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-// 	currentQuestion = availableQuestions[questionsIndex];
-// 	question.innerText = currentQuestion.question;
-
-// 	choices.forEach((choice) => {
-// 		const number = choice.dataset["number"];
-// 		choice.innerText = currentQuestion["choice" + number];
-// 	});
-
-// 	availableQuestions.splice(questionsIndex, 1);
-
-// 	acceptingAnswers = true;
-// };
-
-// choices.forEach((choice) => {
-// 	choice.addEventListener("click", (e) => {
-// 		if (!acceptingAnswers) return;
-
-// 		acceptingAnswers = false;
-// 		const selectedChoice = e.target;
-// 		const selectedAnswer = selectedChoice.dataset["number"];
-
-// 		let classToApply =
-// 			selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-// 		if (classToApply == "correct") {
-// 			incrementScore(SCORE_POINTS);
-// 		}
-
-// 		selectedChoice.parentElement.classList.add(classToApply);
-
-// 		setTimeout(() => {
-// 			selectedChoice.parentElement.classList.remove(classToApply);
-// 			getNewQuestion();
-// 		}, 1000);
-// 	});
-// });
-
-// incrementScore = (num) => {
-// 	score += num;
-// 	scoreText.innerText = score;
-// };
-
-// startGame();
+fetchJSONFile("./src/data/24-2r-t.json");
