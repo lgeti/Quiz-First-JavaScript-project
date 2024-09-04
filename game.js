bbf2b8e32e89af487c2e5fce37bf0c62e61e3f95
igeti <ignat.pocevski@gmail.com>
2024-09-04 13:22:22 +0200

    Created the first iteration of the quiz

--- game.js
+++ game.js
@@ -4,6 +4,19 @@ const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
 
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const answer = document.querySelector("#answer");
+const answered = document.querySelector("#answered");
+const gradeBtns = document.querySelector("#gradeContainer");
+const correctBtn = document.querySelector("#correctBtn");
+const incorrectBtn = document.querySelector("#incorrectBtn");
+
+const SCORE_POINTS = 100;
+let score = 0;
+
+gradeBtns.classList.add("hidden");
+
 const myRequest = new Request("./src/data/24-1r-t.json");
 
 
@@ -18,17 +31,15 @@ function fetchJSONFile(path) {
 		.then((data) => {
 			const questions = data;
 
-			const SCORE_POINTS = 100;
-			const MAX_QUESTIONS = 4;
+			const MAX_QUESTIONS = questions.length;
 			
-			let score = 0;
 
 			startGame = () => {
 				questionCounter = 0;
 				score = 0;
 				availableQuestions = [...questions];
 
-				// getNewQuestion();
+				getNewQuestion();
 			};
 
 			getNewQuestion = () => {
@@ -38,23 +49,46 @@ function fetchJSONFile(path) {
 					return window.location.assign("/end.html");
 				}
 
+				input.classList.remove("hidden");
+				submit.classList.remove("hidden");
+
+				gradeBtns.classList.add("hidden");
+
 				questionCounter++;
-				progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+				progressText.innerText = `${questionCounter} of ${MAX_QUESTIONS}`;
 				progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
 				const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
 				currentQuestion = availableQuestions[questionsIndex];
 				question.innerText = currentQuestion.question;
 
-				choices.forEach((choice) => {
-					const number = choice.dataset["number"];
-					choice.innerText = currentQuestion["choice" + number];
-				});
-
 				availableQuestions.splice(questionsIndex, 1);
 
 				acceptingAnswers = true;
 			};
+
+			submit.addEventListener("click", (e) => {
+				e.preventDefault();
+
+				console.log(currentQuestion.GPTanswer);
+
+				if (!acceptingAnswers) return;
+
+				acceptingAnswers = false;
+
+				const correct = currentQuestion.answer;
+
+				answered.innerText = input.value;
+				input.value = "";
+				input.classList.add("hidden");
+				submit.classList.add("hidden");
+				answer.innerText = correct;
+
+				answered.classList.remove("hidden");
+				answer.classList.remove("hidden");
+
+				gradeBtns.classList.remove("hidden");
+			});
 
 			startGame();
 
@@ -64,6 +98,24 @@ function fetchJSONFile(path) {
 			console.error(error);
 		});
 }
+
+function incrementScore() {
+	score += SCORE_POINTS;
+	scoreText.innerText = score;
+
+	answer.classList.add("hidden");
+	answered.classList.add("hidden");
+
+	getNewQuestion();
+}
+
+function decrementScore() {
+	answer.classList.add("hidden");
+	answered.classList.add("hidden");
+
+	getNewQuestion();
+}
+
 
 fetchJSONFile("./src/data/24-1r-t.json");
 
